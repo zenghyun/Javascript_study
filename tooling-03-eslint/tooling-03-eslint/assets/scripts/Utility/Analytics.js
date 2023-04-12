79cb548e393085fb6d3bb24ad49a161387c292be
zenghyun <pipo36@naver.com>
2023-04-12 17:06:03 +0900

    Work Practice-project

--- tooling-03-eslint/tooling-03-eslint/assets/scripts/Utility/Analytics.js
+++
@@ -1,9 +0,0 @@
-const timerTime = 2000;
-
-const intervalId = setInterval(() => {
-  console.log('Sending analytics data...');
-}, timerTime);
-
-document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-  clearInterval(intervalId);
-});
